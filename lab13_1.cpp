10ed3e198ef983fe607034fb8d66f9972b25c3ac
SK OT <skotskip@gmail.com>
2023-01-10 15:08:23 +0700

    ข้อ_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int sum = 0;
+int N1 = 0,N2 = 1;
+
+int fibonacci(int);
+
+int main()
+{
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x)
+{
+    if(x > 0) fibonacci(x-1);
+
+    if(x == 0) return sum;
+    else if (x == 1) return sum +1;
+    else if (x > 1) 
+    {
+        sum = N1+N2;
+
+        N1 = N2;
+        N2 = sum;
+        return sum;
+    }
+    return 0;
 }
